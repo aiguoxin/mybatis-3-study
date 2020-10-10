efbebc592d81cff7063755174caf7a2b299a1690
艾国信 <aiguoxin@qiyi.com>
2020-10-10 19:17:22 +0800

    增加java8 date测试

---
+++ src/test/java/java8/DateTest.java
@@ -0,0 +1,57 @@
+package java8;
+
+import org.junit.Test;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Date;
+
+/** 2020/10/10 下午6:12 aiguoxin 说明: */
+public class DateTest {
+
+    @Test
+    public void testDate() {
+        Clock clock = Clock.systemDefaultZone();
+        //        System.out.println(clock.millis()+"||"+System.currentTimeMillis());
+        //        获取当前时间
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        System.out.println(localDateTime);
+        System.out.println(new Date());
+
+        // 格式化输出时间
+        DateTimeFormatter dateTimeFormatter =
+                new DateTimeFormatterBuilder()
+                        .appendValue(ChronoField.YEAR)
+                        .appendLiteral("/")
+                        .appendValue(ChronoField.MONTH_OF_YEAR)
+                        .appendLiteral("/")
+                        .appendValue(ChronoField.DAY_OF_MONTH)
+                        .appendLiteral(" ")
+                        .appendValue(ChronoField.HOUR_OF_DAY)
+                        .appendLiteral(":")
+                        .appendValue(ChronoField.MINUTE_OF_HOUR)
+                        .appendLiteral(":")
+                        .appendValue(ChronoField.SECOND_OF_MINUTE)
+                        .toFormatter();
+        System.out.println(localDateTime.format(dateTimeFormatter));
+
+        // 与Date类型互转
+        Date date = new Date();
+        LocalDateTime ldt = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        Date out = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+        System.out.println("ldt=" + ldt.format(dateTimeFormatter) + "|" + "out=" + out);
+
+        // 求两个时间差
+        System.out.println(ldt.with(TemporalAdjusters.firstDayOfMonth()));
+        LocalDateTime nextDate = ldt.plusDays(1);
+        long days = ChronoUnit.DAYS.between(ldt,nextDate);
+        System.out.println("next=" + nextDate.format(dateTimeFormatter) + "| days = " + days);
+    }
+}
---
+++ src/test/java/java8/LambdasTest.java
@@ -0,0 +1,61 @@
+package java8;
+
+import java.lang.annotation.*;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+
+/** 2020/10/9 下午5:07 aiguoxin 说明: */
+public class LambdasTest {
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Filters {
+
+        Filter[] value();
+    }
+
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    @Repeatable(Filters.class)
+    public @interface Filter {
+
+        String value();
+    };
+
+    @Filter("filter1")
+    @Filter("filter2")
+    public interface Filterable {}
+
+//    @Test
+    public void testPrint() {
+        Arrays.asList("a", "b", "d")
+                .forEach(
+                        e -> {
+                            System.out.println(e);
+                        });
+
+        String[] stringArray = {
+            "Barbara", "James", "Mary", "John", "Patricia", "Robert", "Michael", "Linda"
+        };
+        Arrays.sort(stringArray, String::compareTo);
+    }
+
+    //    Scheduled注解就是repeated，方便多次执行不同时间任务
+//    @Test
+    public void testRepeatedAno() {
+        for (Filter annotation : Filterable.class.getAnnotationsByType(Filter.class)) {
+            System.out.println(annotation.value());
+        }
+        for (Filters annotation : Filterable.class.getAnnotationsByType(Filters.class)) {
+            System.out.println(annotation.value());
+        }
+    }
+
+    //    https://zhuanlan.zhihu.com/p/87983832，获取参数名更便捷，比如：MyBatis传递参数
+    public static void main(String[] args) throws Exception {
+        Method method = LambdasTest.class.getMethod("main", String[].class);
+        for (final Parameter parameter : method.getParameters()) {
+            System.out.println("Parameter: " + parameter.getName());
+        }
+    }
+}
