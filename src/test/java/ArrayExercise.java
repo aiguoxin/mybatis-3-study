85687ed6f3f4a1a82dd0b4abc75506906238ae2d
艾国信 <aiguoxin@qiyi.com>
2020-12-12 15:43:35 +0800

    增加数组操作算法

--- src/test/java/ArrayExercise.java
+++ src/test/java/ArrayExercise.java
@@ -1,15 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /** 2020/11/19 下午10:08 aiguoxin 说明: */
 public class ArrayExercise {
     public static void main(String[] args) {
         // 前提：有序数组
-        int[] a = {
-            1, 1, 1, 1, 1, 1, 2, 3, 3,
-        };
-        int length =  rmVal(a,1);
-        System.out.println("length=" + length);
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
-        }
+        //        int[] a = {
+        //            1, 1, 1, 1, 1, 1, 2, 3, 3,
+        //        };
+        //        int length = rmVal(a, 1);
+        //        System.out.println("length=" + length);
+        //        for (int i = 0; i < a.length; i++) {
+        //            System.out.println(a[i]);
+        //        }
+
+        //        int[] nums = {1, 2, 3, 4, 5};
+        //        int[] result = twoSum(nums, 9);
+        //        System.out.println(String.format("%s,%s", result[0], result[1]));
+
+        int initVal = 1534236469;
+        System.out.println(reverse(initVal));
     }
 
     /**
@@ -61,6 +71,7 @@ public class ArrayExercise {
 
     /**
      * 移除指定元素，返回新长度
+     *
      * @param A
      * @param value
      * @return
@@ -68,13 +79,73 @@ public class ArrayExercise {
     public static int rmVal(int[] A, int value) {
         int i = 0;
         int j = 0;
-        while(j < A.length){
-            if(A[j] != value){
+        while (j < A.length) {
+            if (A[j] != value) {
                 A[i] = A[j];
                 i++;
             }
             j++;
         }
         return i;
+    }
+
+
+    public static int[] twoSum(int[] nums, int target) {
+        assert nums.length > 1;
+        Map<Integer, Integer> valMap = new HashMap();
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int findVal = target - nums[i];
+            if (valMap.containsKey(findVal)) {
+                result[0] = valMap.get(findVal);
+                result[1] = i;
+                break;
+            } else {
+                valMap.put(nums[i], i);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/reverse-integer/solution/zheng-shu-fan-zhuan-by-leetcode/
+     * @param x 需要改进：应该使用%10、/10方式模拟出栈入栈
+     * @return  思路不对！！！！
+     */
+    public static int reverse(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        long result = 0;
+        char[] resultChar = String.valueOf(x).toCharArray();
+        int length = resultChar.length;
+        int i = 0;
+        if (x < 0) {
+            i = 1;
+        }
+
+        for (int j = length - 1; j > i; j--) {
+            char temp = resultChar[i];
+            resultChar[i] = resultChar[j];
+            resultChar[j] = temp;
+            i++;
+        }
+        for (int k = resultChar.length - 1; k >= 0; k--) {
+            if (x > 0 || (x < 0 && k > 0)) {
+                result +=
+                        Integer.valueOf(String.valueOf(resultChar[k]))
+                                * Math.pow(10, resultChar.length - k-1);
+            }
+        }
+
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+            return 0;
+        } else {
+            if(x<0){
+                return -Integer.valueOf(String.valueOf(result));
+            }
+            return Integer.valueOf(String.valueOf(result));
+        }
     }
 }
