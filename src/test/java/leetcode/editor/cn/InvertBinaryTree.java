3e5046ac57498c59eebe09725bf4562cb883b36e
艾国信 <aiguoxin@qiyi.com>
2022-04-22 17:44:49 +0800

    tree reverse

---
+++ src/test/java/leetcode/editor/cn/InvertBinaryTree.java
@@ -0,0 +1,117 @@
+package leetcode.editor.cn;//给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [4,2,7,1,3,6,9]
+//输出：[4,7,2,9,6,3,1]
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：root = [2,1,3]
+//输出：[2,3,1]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围在 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 
+// 👍 1260 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import leetcode.bfs.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    //这里使用递归。 深度：借助栈。 广度：借助队列。
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp;
+        temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        //输入：root = [4,2,7,1,3,6,9]
+//输出：[4,7,2,9,6,3,1]
+
+        TreeNode root = new TreeNode(4);
+        TreeNode n1 = new TreeNode(2);
+        TreeNode n2 = new TreeNode(7);
+        TreeNode n3 = new TreeNode(1);
+        TreeNode n4 = new TreeNode(3);
+        TreeNode n5 = new TreeNode(6);
+        TreeNode n6 = new TreeNode(9);
+
+
+        root.left = n1;
+        root.right = n2;
+
+        n1.left = n3;
+        n1.right = n4;
+
+        n2.left = n5;
+        n2.right = n6;
+
+        TreeNode res = invertTree(root);
+        print(res);
+    }
+
+    private static void print(TreeNode res) {
+        if (res != null) {
+            System.out.println(res.val);
+        }
+        if (res.left != null) {
+            print(res.left);
+        }
+        if (res.right != null) {
+            print(res.right);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
